fe86abc00dbafd90f4af6aabfe1567fb89d15017
Grigorii Zimin <zima.grisha@gmail.com>
2025-12-05 05:10:07 +0300

    Update

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,8 +1,10 @@
 import github from '../assets/github.svg';
 import barbershop from '../assets/barbershop.png';
 import lego from '../assets/Lego.png';
+import food from '../assets/food.png'
+import weather from '../assets/weather.png';
+import catalog from '../assets/Catalog.png'
 import dApp from '../assets/dApp.png';
-
 
 export const Projects = () => {
     const projects = [
@@ -21,45 +23,64 @@ export const Projects = () => {
             link: 'https://github.com/Grisha777/Shop',
         },
         {
+            title: 'Рецепты блюд',
+            description: 'Просмотр рецептов блюд, а также добавление новых рецептов',
+            image: food,
+            technologies: ['HTML5', 'CSS3', 'JavaScript', 'Vue', 'Vite', 'Api'],
+            link: 'https://github.com/Grisha777/Food',
+        },
+        {
+            title: 'Погода',
+            description: 'Просмотр погоды в разных городах мира',
+            image: weather,
+            technologies: ['HTML5', 'Tailwind CSS', 'JavaScript', 'Vue', 'Vite', 'Api'],
+            link: 'https://github.com/Grisha777/Weather',
+        },
+        {
+            title: 'Каталог фильмов',
+            description: 'Просмотр информации о фильмах',
+            image: catalog,
+            technologies: ['HTML5', 'Sass', 'TypeScript', 'Angular',],
+            link: 'https://github.com/Grisha777/Catalog',
+        },
+        {
             title: 'Децентрализованное приложение для управления недвижимостью',
             description: 'DApp, обеспечивающее размещение объектов недвижимости, проверку права собственности и проведение сделок на основе технологии блокчейн.',
             image: dApp,
-            technologies: ['TypeScript', 'React', 'Tailwind', 'MetaMask', 'Ethers.js', 'Solidity', 'Hardhat','NodeJS','Pinata', 'Vite'],
+            technologies: ['TypeScript', 'React', 'Tailwind CSS', 'MetaMask', 'Ethers.js', 'Solidity', 'Hardhat','NodeJS','Pinata', 'Vite'],
             link: 'https://github.com/Grisha777/Real-estate-dapp',
         },
     ]
-
     return (
         <section id="projects" className="py-20 bg-gray-700">
             <div className="container mx-auto px-4">
                 <h2 className="text-3xl md:text-4xl font-bold text-center mb-10 text-white">
                     Мои проекты
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {projects.map((project, index) => (
-                    <div key={index}
-                    className="bg-gray-800 rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 border border-gray-500">
-                        <div className="h-60 relative">
-                            <img src={project.image} alt={project.title} className="w-full h-full object-cover"/>
+                <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+                    {projects.map((project, index) => (
+                    <div
+                        key={index}
+                        className="bg-gray-800 rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 border border-gray-500">
+                        <div className="">
+                            <img src={project.image} alt={project.title} className="w-full h-auto object-cover"/>
                         </div>
                         <div className="p-6">
                             <h3 className="text-xl font-bold mb-4 text-white">
                                 {project.title}
                             </h3>
-                            <p className="text-gray-300 mb-4 h-16">
-                                {project.description}
-                            </p>
+                            <p className="text-gray-300 mb-4 h-16">{project.description}</p>
                             <div className="flex flex-wrap gap-2 my-6">
-                            {project.technologies.map((tech, i) => (
+                                {project.technologies.map((tech, i) => (
                                 <span key={i}
-                                className="px-3 py-1 bg-gray-700  text-gray-200 rounded-full text-sm hover:bg-blue-500/20">
+                                    className="px-3 py-1 bg-gray-700  text-gray-200 rounded-full text-sm hover:bg-blue-500/20">
                                     {tech}
                                 </span>
-                            ))}
+                                ))}
                             </div>
                             <a href={project.link} target="_blank" rel="noopener noreferrer"
-                            className="inline-flex items-center justify-center px-6 py-2 bg-cyan-600 text-white rounded-lg hover:bg-cyan-700 transition-colors">
-                                <img src={github} alt="GitHub" className="w-5 h-5 mr-2" />
+                                className="inline-flex items-center justify-center px-6 py-2 bg-cyan-600 text-white rounded-lg hover:bg-cyan-700 transition-colors">
+                                <img src={github} alt="GitHub" className="w-5 h-5 mr-2"/>
                                 Code
                             </a>
                         </div>
@@ -68,5 +89,5 @@ export const Projects = () => {
                 </div>
             </div>
         </section>
-    )
+    );
 }
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -5,21 +5,30 @@ import gitIcon from '../assets/git.svg';
 import figmaIcon from '../assets/figma.svg';
 import TsIcon from '../assets/typescript.svg';
 import ReactIcon from '../assets/react.svg';
+import VueIcon from '../assets/vue.svg';
+import AngularIcon from '../assets/angular.svg';
+import Sass from '../assets/sass.svg';
 import TailwindCSS from '../assets/tailwind.svg';
 import ViteIcon from '../../public/vite.svg';
+import NodeJs from '../assets/nodejs.svg';
+
 
 export const Skills = () => {
     const skills = [
-        { name: 'Git', icon: gitIcon, bgClass: 'bg-orange-500/20' },
-        { name: 'HTML5', icon: htmlIcon, bgClass: 'bg-red-500/20' },
-        { name: 'CSS3', icon: cssIcon, bgClass: 'bg-blue-500/20' },
-        { name: 'Figma', icon: figmaIcon, bgClass: 'bg-purple-500/20' },
-        { name: 'JavaScript', icon: jsIcon, bgClass: 'bg-yellow-500/20' },
-        { name: 'TypeScript', icon: TsIcon, bgClass: 'bg-blue-500/40' },
-        { name: 'React', icon: ReactIcon, bgClass: 'bg-blue-400/20' },
-        { name: 'Tailwind CSS', icon: TailwindCSS, bgClass: 'bg-cyan-400/20' },
-        { name: 'Vite', icon: ViteIcon, bgClass: 'bg-purple-500/20' },
-    ]
+      { name: 'Git', icon: gitIcon, bgClass: 'bg-orange-500/20' },
+      { name: 'HTML5', icon: htmlIcon, bgClass: 'bg-red-500/20' },
+      { name: 'CSS3', icon: cssIcon, bgClass: 'bg-blue-500/20' },
+      { name: 'Figma', icon: figmaIcon, bgClass: 'bg-purple-500/20' },
+      { name: 'JavaScript', icon: jsIcon, bgClass: 'bg-yellow-500/20' },
+      { name: 'TypeScript', icon: TsIcon, bgClass: 'bg-blue-500/40' },
+      { name: 'React', icon: ReactIcon, bgClass: 'bg-blue-400/20' },
+      { name: 'Vue', icon: VueIcon, bgClass: 'bg-green-400/20' },
+      { name: 'Angular', icon: AngularIcon, bgClass: 'bg-red-400/20' },
+      { name: 'SaSS', icon: Sass, bgClass: 'bg-pink-400/20' },
+      { name: 'Tailwind CSS', icon: TailwindCSS, bgClass: 'bg-cyan-400/20' },
+      { name: 'Vite', icon: ViteIcon, bgClass: 'bg-purple-500/20' },
+      { name: 'NodeJS', icon: NodeJs, bgClass: 'bg-green-500/20' },
+    ];
 
     return (
         <section  id="skills" className="py-24 bg-gray-800">
@@ -30,7 +39,7 @@ export const Skills = () => {
                 <p className="text-xl text-center mb-16 text-gray-300">
                     Технологии, которыми я владею
                 </p>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
                 {skills.map((skill, index) => (
                     <div key={index}
                         className="bg-gray-700 p-4 rounded-lg flex flex-col items-center space-y-3 shadow-sm hover:shadow-md transition-shadow">
